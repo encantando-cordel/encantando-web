bdd03d979848b350e4c799b0e2145746fadab9a1
emaanuelsilvaa <emanuelborgesdasilva@gmail.com>
2018-11-07 20:16:30 -0200

    Add Sextilha info screen

---
+++ src/components/SextilhaInfo/SextilhaInfo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class SextilhaInfo extends Component {
+
+    render() {
+        return (
+            <div className = "main">
+            	<p> Teste </p>
+            </div>
+        )
+    }
+}
