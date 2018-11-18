9eb5a0137fe4495bacbf6d955fb30271fd97eabd
Sarah <sarahraqueld@gmail.com>
2018-11-18 18:07:29 -0300

    Change editor background color dynamically

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -7,22 +7,46 @@ import Star from '../../images/star.png';
 import Fish from '../../images/fish.gif';
 import LivroBranco from '../../images/LivroBranco.png';
 import LivroAzul from '../../images/LivroAzul.png';
-import LivroMarron from '../../images/LivroMarron.png';
+import LivroMarrom from '../../images/LivroMarrom.png';
 import LivroVerde from '../../images/LivroVerde.png';
 
 export default class Editor extends Component {
 	
 	constructor(props){
 		super(props);
-		this.state = LivroBranco;
-		this.handleClick = this.handleClick.bind(this);
+		this.state = {
+			color: LivroBranco
+		};
+		this.handleBackgroundColorChange = this.handleBackgroundColorChange.bind(this);
+		this.backgroundAvailableColors = [
+			{name: 'Branco', href: LivroBranco, class: 'bkbranco'}, 
+			{name: 'Azul', href: LivroAzul, class: 'bkazul'}, 
+			{name: 'Verde', href: LivroVerde, class: 'bkverde'}, 
+			{name: 'Marrom', href: LivroMarrom, class: 'bkmarrom'}
+		]
 	}
 			
-	handleClick() {
-		this.state = LivroVerde;
+	handleBackgroundColorChange(color) {
+		this.setState({color: color})
 	}
 
-	
+	createColors = () => {
+		let table = []
+		let children = []
+		for (let i = 0; i < this.backgroundAvailableColors.length; i++) {
+			let color = this.backgroundAvailableColors[i]
+			children.push(
+			<td className={color.class}>
+				<a onClick={(e) => this.handleBackgroundColorChange(color.href, e)}>
+				{color.name}
+				</a>
+			</td>)
+		}
+		table.push(<tr>{children}</tr>)
+		return table
+	}
+
+
     render() {
 		var style = this.state;
 
@@ -32,30 +56,19 @@ export default class Editor extends Component {
 		
         return (
 			<div>
-				<div className="row">
-					{/* Menu esquerdo */}
-					
-					<div className="col-3">
+				<div className="row">					
+					<div className="col-3 left-menu">
 						<br/>
 						<table className="cores">
 							<tbody>
 								<tr>
                                     <th colSpan="4">Selecione a cor de fundo</th>
                                 </tr>
-								<tr className="cores">
-                                    <td className="bkbranco">
-                                        <a onClick={this.handleClick}>Branco</a>
-                                    </td>
-                                    <td className="bkmarron">Marron</td>
-								</tr>
-								<tr className="cores">
-                                    <td className="bkverde">Verde</td>
-                                    <td className="bkazul">Azul</td>
-                                </tr>
+								{ this.createColors() }
 							</tbody>
 						</table>
 						<br/>
-						<h5 class="text-center">Adicione xilogravuras</h5>
+						<h5 className="text-center">Adicione xilogravuras</h5>
 							<div className="xilografias">
 								<img id = "PeopleLeft"  src = { PeopleLeft } width="50" height="50"/>
 								<img id = "PeopleRight"  src = { PeopleRight }  width="50" height="50"/>
@@ -64,15 +77,12 @@ export default class Editor extends Component {
 							</div>				
 					</div>	
 					
-					{/* Editor */}
-					<div className="col-9">
+					<div className="col-9 editor">
 						<br/>
-						<input class="cordel-title text-center" type="text" placeholder="Título do seu cordel" size="80"/>
-							<div className="white-background" style={ {backgroundImage: 'url(' + style + ')'}} >
+						<input className="cordel-title text-center" type="text" placeholder="Título do seu cordel" size="80"/>
+							<div className="white-background" style={ {backgroundImage: 'url(' + this.state.color + ')'}} >
 								<div className="text-container">
 									<textarea></textarea>
-								</div>
-								<div className="editortexto">
 								</div>
 							</div>
 					</div>			   
