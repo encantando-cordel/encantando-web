3853922057022ce9818483ce580baeff111f64b2
Ewerton Leandro <3w3rt0n@gmail.com>
2018-11-22 11:40:39 -0300

    Versão final do editor, alterado estrutura, layout e função trocar background

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -5,85 +5,155 @@ import PeopleRight  from '../../images/peopleRight.png';
 import PeopleLeft from '../../images/peopleLeft.png';
 import Star from '../../images/star.png';
 import Fish from '../../images/fish.gif';
-import LivroBranco from '../../images/LivroBranco.png';
-import LivroAzul from '../../images/LivroAzul.png';
-import LivroMarron from '../../images/LivroMarron.png';
-import LivroVerde from '../../images/LivroVerde.png';
+import Livro from '../../images/book.png';
+
+import btnVoltar from '../../images/progress-button.png';
+import btnExemplos from '../../images/progress-button.png';
+import btnAjuda from '../../images/progress-button.png';
+import btnConcluir from '../../images/botao_concluir_editor.png';
 
 export default class Editor extends Component {
 	
 	constructor(props){
 		super(props);
-		{/* this.state = { color: 'red'}; */}
-		this.state = LivroBranco;
+		this.state = { color: 'white'}; 
 		
-		this.handleClick = this.handleClick.bind(this);
+		this.bkBranco = this.bkBranco.bind(this);
+		this.bkMarron = this.bkMarron.bind(this);
+		this.bkAmarelo = this.bkAmarelo.bind(this);
+		this.bkRosa = this.bkRosa.bind(this);
 	}
 			
-	handleClick() {
-		console.log('hello world');
-		{/* this.setState({ color: 'blue' }); */}
+	bkBranco() {
+		this.setState({ color: 'white' });	
 		console.log(this.state);
-		this.state = LivroVerde;
+	}
+	
+	bkMarron(){
+		this.setState({ color: '#c69c6d' });	
+		console.log(this.state);
+	}
+	
+	bkAmarelo(){
+		this.setState({ color: '#fff899' });	
+		console.log(this.state);
+	}
+	
+	bkRosa(){
+		this.setState({ color: '#f79779' });	
 		console.log(this.state);
 	}
 
 	
     render() {
 		console.log('rendering');
-		var style = this.state;
-		/*{
-			backgroundColor: this.state.color 
-		  
-		};*/
+		var style = {
+			backgroundColor: this.state.color		  
+		};
 
 		if (this.props.withsemi) {
-			/* style.backgroundColor += ";"; */
-			style.backgroundImage += ";";
+			style.backgroundColor += ";"; 
 		}
 		
         return (
-			<div>
-				<div className="row">
+			<div className="main">
+			
+				{/* Menu Superior*/}
+				<div className="menuTop">
+				
+					{/* Menu para selecionar as cores de background*/}
+					
+					<div >
+						<h2>COR DO FUNDO</h2>
+						{/* Tabela com as cores de background */}
+						<div className="menuBackground">
+							<table>
+								<tbody>									
+									<tr>
+										<td className="bkbranco" onClick={this.bkBranco}></td>
+										<td className="bkmarron" onClick={this.bkMarron}></td>
+										<td className="bkamarelo" onClick={this.bkAmarelo}></td>
+										<td className="bkrosa" onClick={this.bkRosa}></td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					
+					<h2>CRIANDO SEU CORDEL</h2>	
+					
+					{/* Botão Ajudar */}
+					<button className="btnAjuda" onClick=""><img src={ btnAjuda } alt="Ajuda" /></button>
+					{/* Remover esse trecho após validação
+					<a href="/ajudar"  title="Ajudar">
+						<img src={ btnVoltar } alt="Ajudar" className="btnAjuda"/>
+					</a>
+					*/}
+				</div>
+				
+				{/* Tela Principal */}
+				<div className="editor">
 					{/* Menu esquerdo */}
-					<div className="column">
+					<div className="menuLeft">		
+						
+						{/* Menu das xilogravuras */}
 						<br/>
-						<table className="cores">
-							<tbody>
-								<tr><th colSpan="4">Selecione a cor de fundo.</th></tr>
-								<tr className="cores"><td className="bkbranco"><a onClick={this.handleClick}>Branco</a></td><td className="bkmarron">Marron</td><td className="bkverde">Verde</td><td className="bkazul">Azul</td></tr>
-							</tbody>
-						</table>
-						<br/>
-						<h2>Xilogrfias</h2>
-							<div className="xilografias">
-								<img id = "PeopleLeft"  src = { PeopleLeft } width="150" height="150"/>
-								<img id = "PeopleRight"  src = { PeopleRight }  width="150" height="150"/>
-								<img id = "Star"  src = { Star }  width="150" height="100"/>
-								<img id = "Fish"  src = { Fish }  width="150" height="100"/>
-							</div>				
+						<h2>XILOGRAVURAS</h2>
+						<div className="menuXilogravuras">
+							{/* Imagens das Xilogravuras */}
+							<img id = "PeopleLeft"  src = { PeopleLeft } width="100" height="100"/>
+							<img id = "PeopleRight"  src = { PeopleRight }  width="100" height="100"/>
+							<img id = "Star"  src = { Star }  width="100" height="100"/>
+							<img id = "Fish"  src = { Fish }  width="100" height="100"/>
+						</div>				
 					</div>	
-					{/* Editor */}
-					<div className="columnCenter">
-						<br/>
-						<h2>Título do seu Cordel</h2>	
-							<div className="background1" style={ {backgroundImage: 'url(' + style + ')'}} >
-								<div className="editorxilografia">
-									<p>aa</p>
+					
+					{/* Area do Editor */}
+					<div className="areaEditor">
+						
+							<div className="background" style={ style } >
+							
+								{/* Titulo do cordel */}
+								<div clasName="editorTitulo">
+									<input type="text" value="Digite aqui o TÍTULO"/><br/>
 								</div>
-								<div className="editortexto">
 								
+								{/* Area para coloca as xilogravuras */}
+								<div className="editorXilogravuras">
+								</div>
+								
+								{/* Area para digita os veros */}
+								<div className="editorTexto">
+									<textarea rows="10" cols="50">
+										Digite aqui o VERSOS de seu cordel!
+									</textarea>
 								</div>
 							</div>
 					</div>			   
 			   </div>
-			   <div className="bottom">
-					<table className="menubottom">	
-						<tbody>
-							<tr><td>Voltar</td><td>Exemplos</td><td>Ajuda</td><td>Concluir</td></tr>
-						</tbody>
-					</table>
-			   </div>
+			   
+			   {/* Menu inferior */}
+			   <div className="menuBottom">
+				
+					{/* Botão Voltar */}
+					<a href="/home"  title="Voltar">
+						<img src={ btnVoltar } alt="Voltar" className="btn"/>
+					</a>
+					
+					{/* Botão Exemplos */}
+					<a href="/exemplos"  title="Exemplos">
+					  <img src={ btnExemplos } alt="Exemplos" className="btn"/>
+					</a>					
+					
+					{/* Botão Concluir */}
+					<button className="btnConcluir" onClick=""><img src={ btnConcluir } alt="Concluir" /></button>
+				    {/* Remover esse trecho após validação
+					<a href="/concluir"  title="Concluir">
+					  <img src={ btnConcluir } alt="Concluir" className="btnConcluir"/>
+					</a>
+					*/}
+				</div>	
+				
 			</div>			
 		)
     }
