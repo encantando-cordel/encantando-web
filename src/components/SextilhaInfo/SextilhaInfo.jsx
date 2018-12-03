d8d67379c201528b5d51d7d1e7dc2ddd8ee873f0
Emanuel Silva <emanuelborgesdasilva@gmail.com>
2018-12-03 16:53:15 -0200

    Indentação OK

--- src/components/SextilhaInfo/SextilhaInfo.jsx
+++ src/components/SextilhaInfo/SextilhaInfo.jsx
@@ -7,17 +7,16 @@ import bntVoltarSext from "../../images/botaovoltar0.png";
 export default class SextilhaInfo extends Component {
     render() {
     	return (
-            
-            <div className = "mainSextInfo">
+	<div className = "mainSextInfo">
             	<div className="generalToSext">
-      				<a className="nextToSext" href="/novo" title="Novo" >
-		    			<img src={btnAvancarSext} alt="Novo" className="btn" />
-		 			</a>	
-      				<a className="backToSext" href="/character" title="back" >
-		    			<img src={bntVoltarSext} alt="back" className="btn" />
-		 			</a>
-				</div>
-          	</div>
+      			<a className="nextToSext" href="/novo" title="Novo" >
+		    		<img src={btnAvancarSext} alt="Novo" className="btn" />
+		 	</a>	
+      			<a className="backToSext" href="/character" title="back" >
+		    		<img src={bntVoltarSext} alt="back" className="btn" />
+		 	</a>
+		</div>
+	</div>
         )
     }
 }
